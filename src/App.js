ce2fef782fed6287a53ba1347d8f93ab8cfd7473
Liz Rowe <83372467+lizzyrowe1107@users.noreply.github.com>
2022-02-28 10:30:19 -0800

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from It's Just Liz!
+          Hello from Liz!!
         </p>
         <a
           className="App-link"

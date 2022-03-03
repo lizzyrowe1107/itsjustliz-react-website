00c1f49d5dff466f558e05f62caa8b841ed03111
Liz Rowe <83372467+lizzyrowe1107@users.noreply.github.com>
2022-03-02 19:07:03 -0800

    Edit Structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from Liz!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="row">
+          <div className="col-6">
+            <img src={logo} className="App-logo" alt="logo" /> I'm Liz, a Mechanical Engineer and YouTuber
+          </div>
+          <div className="col-6">
+            <code>insert photo</code>
+          </div>
+
+        </div>
+        
       </header>
+
+      <main>
+
+        
+
+      </main>
+
+      <footer>
+        
+        
+
+      </footer>
+
+        
+        
     </div>
   );
 }

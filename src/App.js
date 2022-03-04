f5f3e2d027d7e8000c69d6d8f86f87744dc49a76
Liz Rowe <83372467+lizzyrowe1107@users.noreply.github.com>
2022-03-03 19:32:23 -0800

    Header

--- src/App.js
+++ src/App.js
@@ -8,11 +8,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="row">
-          <div className="col-6">
-            <img src={logo} className="App-logo" alt="logo" /> I'm Liz, a Mechanical Engineer and YouTuber
+
+          <div className="col-6 mt-5 img-description">
+            <img src={logo} className="App-logo" alt="logo" /> 
+            <span className="profile-description">
+
+              I'm Liz, a Mechanical Engineer and <a className="youtube-link"href="https://www.youtube.com/channel/UCvUUPRqQxlSjXiJQsIU1HhA">YouTuber</a>
+            </span>
+            <h3 class="about-description mt-5">
+              Explore my website to find out information on my book reviews, projects, and construction experience. 
+            </h3>
+
           </div>
-          <div className="col-6">
-            <img src={TwitterBackground}></img>
+
+          <div className="col-lg-6">
+            <div className="profile-picture">
+              <img className="img-fluid rounded mt-5" src={TwitterBackground}></img>
+
+            </div>
           </div>
 
         </div>
